9d6346dcf711a877e6de5b8c48251b94a7e9766a
Akash M Nandan <akash003nandan@gmail.com>
2024-12-22 07:30:13 +0530

    ScrollView component

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Image, ImageBackground, Text, View } from "react-native";
+import { Image, ImageBackground, ScrollView, Text, View } from "react-native";
 
 // importing images
 const bgImage = require("@/assets/images/adaptive-icon.png");
@@ -29,11 +29,13 @@ export default function Index() {
           justifyContent: "center",
           padding: 20,
         }}>
-          <Text>
-            <Text style={{color: "white", fontSize: 20}}>Hello</Text> , world!
-          </Text>
-          <Image source={{uri: "https://picsum.photos/300"}} style={{width: 150, height: 150}} />
-          
+          <ScrollView>
+            <Text>
+              <Text style={{color: "white", fontSize: 20}}>Hello</Text> , world!
+            </Text>
+            <Image source={{uri: "https://picsum.photos/300"}} style={{width: 150, height: 150}} />
+            <Text>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</Text>
+          </ScrollView>
         </View>
 
         <View style={{
