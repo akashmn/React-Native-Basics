cced3e0b5c5e76c8222e92397c49b996b9772831
Akash M Nandan <akash003nandan@gmail.com>
2024-12-22 07:05:27 +0530

    Text component

--- app/index.tsx
+++ app/index.tsx
@@ -5,12 +5,22 @@ export default function Index() {
     <View style={{
       backgroundColor: "plum",
       flex: 1,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      gap: 20,
     }}>
       <View style={{
         backgroundColor: "lightblue",
         width: 200,
         height: 200,
+        display: "flex",
+        justifyContent: "center",
+        padding: 20,
       }}>
+        <Text>
+          <Text style={{color: "white", fontSize: 20}}>Hello</Text> , world!
+        </Text>
         
       </View>
 
@@ -18,7 +28,13 @@ export default function Index() {
         backgroundColor: "lightgreen",
         width: 200,
         height: 200,
+        display: "flex",
+        justifyContent: "center",
+        padding: 20,
       }}>
+        <Text>
+          <Text style={{color: "white", fontSize: 20}}>Goodbye</Text> , world!
+        </Text>
         
       </View>
 
